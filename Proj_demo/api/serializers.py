13a1212d5db2c98666d606b2105de54d70eb6485
Devopssrookie <166130705+Devopssrookie@users.noreply.github.com>
2024-04-22 10:27:43 +0530

    Delete Proj_demo directory

--- Proj_demo/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from members.models import Task
-
-class Memberserializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = '__all__'
--- Proj_demo/members/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path 
-from . import views 
-
-urlpatterns = [
-    path('', views.TaskList, name="tasks"),
-    path('<str:pk>/', views.TaskDetail, name="task"),
-    path('<str:pk>/delete/', views.TaskDelete, name="delete"),
-]
